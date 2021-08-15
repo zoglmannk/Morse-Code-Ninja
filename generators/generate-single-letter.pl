b58ea1beab768bebb265ce65e8f68393ac671c81
Kurt Zoglmann <zoglmannk@yahoo.com>
2021-08-15 13:18:00 -0500

    Added documentation for generating "Mind Melt" practice sets

--- generators/generate-single-letter.pl
+++ generators/generate-single-letter.pl
@@ -8,8 +8,11 @@ use Cwd;
 #
 # Rapid-Fire
 # ./render.pl -i single-letters-rapid-fire.txt -s 15 17 20 22 25 28 30 35 40 45 50 --sm 0.2 -sv 0.3 -ss 0.3
+#
+# Mind Melt
+# ./render.pl -i single-letters-mind-melt.txt -s 15 17 20 22 25 28 30 35 40 45 50 --norepeat --nocourtesytone -ss 0.2 -sm 0.2 -sv 0.2
 
-my $number_of_runs = 2500;
+my $number_of_runs = 5000;
 
 my @letters = ('A'..'Z');
 
