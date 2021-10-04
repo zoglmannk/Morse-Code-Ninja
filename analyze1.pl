c303170b1454b797533165e3de9e2df91e15a69f
Kurt Zoglmann <zoglmannk@yahoo.com>
2021-10-04 12:07:44 -0500

    Crufty closed-captioning changes

--- analyze1.pl
+++ analyze1.pl
@@ -4,29 +4,76 @@ use strict;
 use warnings;
 use POSIX "fmod";
 
-my $min_length = 0.7; #have been using 0.3 (0.3 to 1.0 is often needed)
+my $min_length = 0.5; #have been using 0.3 (0.3 to 1.0 is often needed)
+my $secondary_length = 0.3; # typically 0.1
 
 my $current_position = 0;
 my @positions = (
+    'Voice',
+    'Chirp'
+);
+
+my @positions2 = (
     'Morse',
     'Voice',
     'MorseRepeat',
     'Chirp'
 );
+
+# If using this option for the 4x, 3x, 2x word spacing
+# rm ~/Desktop/analyze/*.vtt; ls -1 ~/Desktop/analyze/ | grep mp3 |  xargs -IXX echo aubioquiet -r 0 -i \"/Users/kaz/Desktop/analyze/XX\" \| ./analyze0.pl \| ./analyze1.pl \"/Users/kaz/Desktop/analyze/Sentences\ from\ Top\ 100\ Words\ -\ 4x\ Word-Spacing\ 15wpm.txt\" \| ./analyze2.pl \"/Users/kaz/Desktop/analyze/Sentences\ from\ Top\ 100\ Words\ -\ 4x\ Word-Spacing\ 15wpm.txt\" \> \"/Users/kaz/Desktop/analyze/XX.vtt\" | perl -e 'while(<>) { $in = $_; $in =~ s/\.mp3\.vtt/.vtt/g; print "$in"; }' > runme.bash; chmod +x runme.bash;
+# Edit runme.bash and match text files to run
+# ./runme.bash
+# change get_position function to reference @position3
+#my @positions3 = ();
+#my $filename   = $ARGV[0];
+#open(FH, '<', $filename) or die $!;
+#while(<FH>) {
+#    my $in = $_;
+#    chomp($in);
+#    my @words = split(/\s+/, $in);
+#    for(@words) {
+#        push @positions3, "Morse";
+#    }
+#    push @positions3, "Voice";
+#    for(@words) {
+#        push @positions3, "Morse";
+#    }
+#    push @positions3, "Chirp";
+#}
+#close FH;
+#print "@positions3\n";
+
+my $which_position = 1;
 sub get_position {
     my $type = $_[0];
 
-    #if($current_position == 190 || $current_position == 1096) {
-    #    $current_position ++;
+    #if( $current_position == 3) {
+    #    $current_position++;
     #}
     #print "   $current_position\n";
 
     if($type eq 'QUIET') {
         return "Quiet";
     } else {
-        my $index = $current_position % scalar(@positions);
-        $current_position++;
-        return $positions[$index];
+        # if($which_position == 1) {
+        #     if($current_position >= scalar(@positions)) {
+        #         $which_position = 2;
+        #         $current_position = 0;
+        #         #print "got here\n";
+        #     } else {
+        #         my $index = $current_position % scalar(@positions);
+        #         $current_position++;
+        #         return $positions[$index];
+        #     }
+        # }
+
+        # if($which_position == 2) {
+            my $index = $current_position % scalar(@positions2);
+            $current_position++;
+            return $positions2[$index];
+        # }
+
     }
 }
 
@@ -56,7 +103,7 @@ my $previous_type = "QUIET";
 my $previous_time_index = 0;
 my $collapsing = 0;
 my $collapsing_start_index = 0;
-while(<>) {
+while(<STDIN>) {
     my $in = $_;
     chomp($in);
 
@@ -69,7 +116,7 @@ while(<>) {
         my $current_length = $next_time_index - $current_time_index;
         #print "   $previous_type  $previous_time_index len:$previous_length  .. incoming: $next_type $next_time_index .. current: $type  len:$current_length => collapsing:$collapsing\n";
 
-        if($previous_type eq 'NOISY' && ($previous_length < $min_length || $current_length < 0.1) && $collapsing == 0) {
+        if($previous_type eq 'NOISY' && ($previous_length < $min_length || $current_length < $secondary_length) && $collapsing == 0) {
             $collapsing = 1;
             $collapsing_start_index = $previous_time_index;
         } elsif($previous_type eq 'QUIET' && $previous_length > $min_length && $collapsing == 1) {
@@ -87,6 +134,11 @@ while(<>) {
             my $formatted_previous_time_index = format_time($previous_time_index);
             my $formatted_current_time_index = format_time($current_time_index);
             print "$position, $previous_type, $previous_time_index - $current_time_index, $previous_length, $formatted_previous_time_index - $formatted_current_time_index\n";
+            # Hack for rare missing repeat
+            #if($previous_length >= 3.3 && $position ne 'Voice') {
+            #    get_position("NOISY");
+            #    #print "Got here\n";
+            #}
         }
 
         $previous_type = $type;
--- analyze2.pl
+++ analyze2.pl
@@ -6,10 +6,20 @@ use POSIX "fmod";
 
 # Usage:
 #   aubioquiet -r 0 -i ~/Desktop/analyze/Single\ Letters\ 50wpm.mp3 | ./analyze0.pl | analyze1.pl | ./analyze2.pl ~/Desktop/analyze/Single\ Letters.txt > ~/Desktop/analyze/single-letters-50wpm.vtt
-#   ls -1 ~/Desktop/analyze/ | grep mp3 |  xargs -IXX echo aubioquiet -r 0 -i \"/Users/kaz/Desktop/analyze/XX\" \| ./analyze0.pl \| ./analyze1.pl \| ./analyze2.pl \"/Users/kaz/Desktop/analyze/Top\ 100\ Words\ Repeated\ v2.txt\" \> \"/Users/kaz/Desktop/analyze/XX.vtt\" > runme.bash; chmod +x runme.bash;
+#   ls -1 ~/Desktop/analyze/ | grep mp3 |  xargs -IXX echo aubioquiet -r 0 -i \"/Users/kaz/Desktop/analyze/XX\" \| ./analyze0.pl \| ./analyze1.pl \| ./analyze2.pl \"/Users/kaz/Desktop/analyze/Top\ 100\ Words\ Repeated\ v2.txt\" \> \"/Users/kaz/Desktop/analyze/XX.vtt\" | perl -e 'while(<>) { $in = $_; $in =~ s/\.mp3\.vtt/.vtt/g; print "$in"; }' > runme.bash; chmod +x runme.bash; ./runme.bash
+#
+# Course
+# ls -1 ~/Desktop/20:10wpm/ | grep txt | perl -e 'while(<>) { $in = $_; chomp($in); if($in !~ m/lesson_\d+a/) { next;}  $in =~ m/^(.*?)\.txt/; $filename = $1; print "aubioquiet -r 0 -i /Users/kaz/Desktop/20:10wpm/${filename}-20wpm.mp3 | ./analyze0.pl | ./analyze1.pl | ./analyze2.pl /Users/kaz/Desktop/20:10wpm/${filename}.txt > /Users/kaz/Desktop/20:10wpm/${filename}.vtt\n"; }' > runme-a.bash; chmod a+rwx runme-a.bash
+# ls -1 ~/Desktop/20:10wpm/ | grep txt | perl -e 'while(<>) { $in = $_; chomp($in); if($in !~ m/lesson_\d+b/) { next;}  $in =~ m/^(.*?)\.txt/; $filename = $1; print "aubioquiet -r 0 -i /Users/kaz/Desktop/20:10wpm/${filename}-20wpm.mp3 | ./analyze0.pl | ./analyze1.pl | ./analyze2.pl /Users/kaz/Desktop/20:10wpm/${filename}.txt > /Users/kaz/Desktop/20:10wpm/${filename}.vtt\n"; }' > runme-b.bash; chmod a+rwx runme-b.bash
+# ls -1 ~/Desktop/20:10wpm/ | grep txt | perl -e 'while(<>) { $in = $_; chomp($in); if($in !~ m/lesson_\d+[cde]/) { next;}  $in =~ m/^(.*?)\.txt/; $filename = $1; print "aubioquiet -r 0 -i /Users/kaz/Desktop/20:10wpm/${filename}-20wpm.mp3 | ./analyze0.pl | ./analyze1.pl | ./analyze2.pl /Users/kaz/Desktop/20:10wpm/${filename}.txt > /Users/kaz/Desktop/20:10wpm/${filename}.vtt\n"; }' > runme-cde.bash; chmod a+rwx runme-cde.bash
+# ls -1 ~/Desktop/20:10wpm/ | grep txt | perl -e 'while(<>) { $in = $_; chomp($in); if($in !~ m/lesson_\d+[fg]/) { next;}  $in =~ m/^(.*?)\.txt/; $filename = $1; print "aubioquiet -r 0 -i /Users/kaz/Desktop/20:10wpm/${filename}-20wpm.mp3 | ./analyze0.pl | ./analyze1.pl | ./analyze2.pl /Users/kaz/Desktop/20:10wpm/${filename}.txt > /Users/kaz/Desktop/20:10wpm/${filename}.vtt\n"; }' > runme-fg.bash; chmod a+rwx runme-fg.bash
 
 # Create Web Video Text Tracks Format (WebVTT) -- https://developer.mozilla.org/en-US/docs/Web/API/WebVTT_API
 # Closed Captioning on YouTube -- https://www.rev.com/blog/close-caption-file-format-guide-for-youtube-vimeo-netflix-and-more
+
+# SST
+# ls -1 ~/Desktop/analyze/ | grep mp3 | grep sst-exchanges |  xargs -IXX echo aubioquiet -r 0 -i \"/Users/kaz/Desktop/analyze/XX\" \| ./analyze0.pl \| ./analyze1.pl \| ./analyze2.pl \"/Users/kaz/Desktop/analyze/sst-exchanges.txt\" \> \"/Users/kaz/Desktop/analyze/XX.vtt\" | perl -e 'while(<>) { $in = $_; $in =~ s/\.mp3\.vtt/.vtt/g; print "$in"; }' > runme.bash; chmod +x runme.bash; ./runme.bash
+
 
 my $filename   = $ARGV[0];
 open(FH, '<', $filename) or die $!;
@@ -130,12 +140,38 @@ while(<STDIN>) {
 
             my $entry = <FH>;
             (my $sentence_part, my $spoken_directive, my $repeat_part) = split_on_spoken_directive($entry);
+
             $spoken_directive = unencoded_speak($spoken_directive);
-            $spoken_directive =~s/\.|,//g; #remove periods
+            #$spoken_directive =~ s/zero/0/g;
+            #if($spoken_directive =~ m/^(([A-Z0-9](\s|,))+)\s(.*)$/) {
+            #    my $prefix = $1;
+            #    my $country = $4;
+            #    #$country = ucfirst(lc($country));
+            #    $country = join " ", map {ucfirst} split " ", lc($country);
+            #    $prefix =~ s/([A-Z0-9])(\s|,)/$1/g;
+            #    $spoken_directive = "$prefix $country";
+            #}
+            #$spoken_directive =~s/\s//g; #remove spaces
+            #$spoken_directive =~s/\.|,//g; #remove periods and commans
+            #$spoken_directive = join " ", split //, uc($spoken_directive);
+            #$spoken_directive =~ s/Q U E S T I O N M A R K/?/g;
+            #$spoken_directive =~ s/S L A S H/\//g;
+            #$spoken_directive =~ s/slash/\//g;
+            #$spoken_directive =~ s/< B T >/&lt;BT&gt;/g;
+            #$spoken_directive =~ s/< B K >/&lt;BK&gt;/g;
+            #$spoken_directive =~ s/P E R I O D/./g;
+
             #$spoken_directive = uc($spoken_directive);
+            #$spoken_directive =~ s/([A-Z]),/\1/g;
+            #$spoken_directive = join " ", map {ucfirst} split " ", lc($spoken_directive);
 
             #print "Voice, $start, $formatted_start_time, $formatted_end_time, $spoken_directive\n";
             print "$formatted_start_time --> $formatted_end_time\n$spoken_directive\n\n";
+            
+            #if($sentence_part eq 'VV') {
+            #    $sentence_part = "New QSO";
+            #}
+            #print "$formatted_start_time --> $formatted_end_time\n$sentence_part\n\n";
 
         }
 
