5d57431444d03d4b238d5f5bb7d5391f1c0a8433
Kurt Zoglmann <zoglmannk@yahoo.com>
2022-08-07 17:56:10 -0500

    Extended functionality of New Word Generator

--- generators/new-word-drill-generator.pl
+++ generators/new-word-drill-generator.pl
@@ -23,10 +23,10 @@ sub find_new_words_for {
     chomp($in);
     $in = lc($in);
 
-    $in =~ s/\.|\?|,|-//g;
+    $in =~ s/\.|\?|,|-|\^|\(|\)//g;
     my @words = split /\s/, $in;
     foreach my $word (@words) {
-      if(! defined $previous_uniq_words{$word} && $word ne 'tqhen') {
+      if(! defined $previous_uniq_words{$word} && $word ne 'tqhen' && $word ne 'abc' && length($word) >= 2) {
         $new_uniq_words{$word} = 1;
         $previous_uniq_words{$word} = 1;
       }
@@ -63,7 +63,7 @@ sub pick_uniq_pangram_index {
       %prev_picked = ();
     }
 
-    if($prev_picked{$next_pick} != 1) {
+    if(! defined $prev_picked{$next_pick}) {
       $prev_picked{$next_pick} = 1;
       return $next_pick;
     }
@@ -76,7 +76,7 @@ sub generate_practice_for_new_words {
   my $write_file_name = $_[0];
 
   $last_picked = "";
-  %prev_picked = {};
+  %prev_picked = qw();
 
   open(my $WRITE_FH, '>', $write_file_name) or die $!;
 
@@ -93,36 +93,58 @@ sub generate_practice_for_new_words {
   close($WRITE_FH);
 }
 
-for(my $i=1; $i <= 16; $i++) {
 
-  my $read_fn = '/Users/kaz/git/Morse-Code-Ninja/practice-sets/Sentences from Top '. $i . '00 words.txt';
-  my $write_fn = '/Users/kaz/git/Morse-Code-Ninja/Sentences-from-Top-'. $i . '00-words-uniq-alt.txt';
+# Generate New word Drills
+# for(my $i=1; $i <= 16; $i++) {
+#
+#   my $read_fn = '/Users/kaz/git/Morse-Code-Ninja/practice-sets/Sentences from Top '. $i . '00 words.txt';
+#   my $write_fn = '/Users/kaz/git/Morse-Code-Ninja/Sentences-from-Top-'. $i . '00-words-uniq-alt.txt';
+#   find_new_words_for($read_fn, $write_fn);
+#   generate_practice_for_new_words('/Users/kaz/git/Morse-Code-Ninja/Sentences-from-Top-'. $i . '00-words-prepare-alt.txt');
+#
+#   # if($i == 10) {
+#   #   for(my $j=1; $j <= 7; $j++) {
+#   #     my $read_fn = "/Users/kaz/git/Morse-Code-Ninja/practice-sets/Sentences from Top 1000 Words - Review Part $j.txt";
+#   #     my $write_fn = "/Users/kaz/git/Morse-Code-Ninja/Sentences-from-Top-1000-Words-Review-Part-${j}-uniq.txt";
+#   #     find_new_words_for($read_fn, $write_fn);
+#   #     generate_practice_for_new_words("/Users/kaz/git/Morse-Code-Ninja/Sentences-from-Top-1000-Words-Review-Part-${j}-prepare.txt");
+#   #   }
+#   # }
+#
+# }
+#
+# for(my $i=1; $i <= 16; $i++) {
+#
+#   print "./render.pl -i Sentences-from-Top-${i}00-words-prepare-alt.txt -s 15 17 20 22 25 28 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 --sm 0.5 --ss 0.5 --sv 0.5; ";
+#
+#   # if($i == 10) {
+#   #   for (my $j = 1; $j <= 7; $j++) {
+#   #     print "./render.pl -i Sentences-from-Top-${i}000-Words-Review-Part-{j}-prepare.txt -s 15 17 20 22 25 28 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 --sm 0.5 --ss 0.5 --sv 0.5; ";
+#   #   }
+#   # }
+#
+# }
+
+
+%previous_uniq_words = qw();
+%new_uniq_words = qw();
+
+my @n_gram_sets = (
+    "Sets of 2 Words",
+    "Sets of 3 Words",
+    "Sets of 4 Words",
+    "Sets of 5 Words",
+    "Sets of 5 Words Encore",
+    "Sets of 5 Words Encore2",
+);
+
+foreach my $n_gram_set (@n_gram_sets) {
+  my $read_fn = "/Users/kaz/git/Morse-Code-Ninja/practice-sets/${n_gram_set}.txt";
+  my $write_fn = "/Users/kaz/git/Morse-Code-Ninja/${n_gram_set} - Unique Words.txt";
   find_new_words_for($read_fn, $write_fn);
-  generate_practice_for_new_words('/Users/kaz/git/Morse-Code-Ninja/Sentences-from-Top-'. $i . '00-words-prepare-alt.txt');
+  generate_practice_for_new_words("/Users/kaz/git/Morse-Code-Ninja/${n_gram_set} - Word Drill.txt");
 
-  # if($i == 10) {
-  #   for(my $j=1; $j <= 7; $j++) {
-  #     my $read_fn = "/Users/kaz/git/Morse-Code-Ninja/practice-sets/Sentences from Top 1000 Words - Review Part $j.txt";
-  #     my $write_fn = "/Users/kaz/git/Morse-Code-Ninja/Sentences-from-Top-1000-Words-Review-Part-${j}-uniq.txt";
-  #     find_new_words_for($read_fn, $write_fn);
-  #     generate_practice_for_new_words("/Users/kaz/git/Morse-Code-Ninja/Sentences-from-Top-1000-Words-Review-Part-${j}-prepare.txt");
-  #   }
-  # }
-
+  print "./render.pl -i \"${n_gram_set} - Word Drill.txt\" -s 15 17 20 22 25 28 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 --sm 0.5 --ss 0.5 --sv 0.5; \n";
 }
-
-for(my $i=1; $i <= 16; $i++) {
-
-  print "./render.pl -i Sentences-from-Top-${i}00-words-prepare-alt.txt -s 15 17 20 22 25 28 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 --sm 0.5 --ss 0.5 --sv 0.5; ";
-
-  # if($i == 10) {
-  #   for (my $j = 1; $j <= 7; $j++) {
-  #     print "./render.pl -i Sentences-from-Top-${i}000-Words-Review-Part-{j}-prepare.txt -s 15 17 20 22 25 28 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 --sm 0.5 --ss 0.5 --sv 0.5; ";
-  #   }
-  # }
-
-}
-
-
 
 
