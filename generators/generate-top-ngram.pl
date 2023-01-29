eb3f1c39db633080d9323528f07633915a6dc1ca
Kurt Zoglmann <zoglmannk@yahoo.com>
2023-01-29 16:26:47 -0600

    Documented Extra Word Space Timing on n-gram practice sets

--- generators/generate-top-ngram.pl
+++ generators/generate-top-ngram.pl
@@ -12,6 +12,26 @@ open my $in, "<", "data/2ngram-top.txt";
 #open my $in, "<", "data/4ngram-top.txt";
 #open my $in, "<", "data/5ngram-top.txt";
 
+# Farnsworth timing - Extra Word Spacing
+# 2 Words
+# ./render.pl -i Sets-of-2-Words-4x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 3 --sm 0.2
+# ./render.pl -i Sets-of-2-Words-3x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 2 --sm 0.5
+# ./render.pl -i Sets-of-2-Words-2x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 1 --sm 0.7
+#
+# 3 Words
+# ./render.pl -i Sets-of-3-Words-4x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 3 --sm 0.3
+# ./render.pl -i Sets-of-3-Words-3x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 2 --sm 0.6
+# ./render.pl -i Sets-of-3-Words-2x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 1 --sm 0.8
+#
+# 4 Words
+# ./render.pl -i Sets-of-4-Words-4x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 3 --sm 0.4
+# ./render.pl -i Sets-of-4-Words-3x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 2 --sm 0.7
+# ./render.pl -i Sets-of-4-Words-2x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 1 --sm 0.9
+#
+# 5 Words
+# ./render.pl -i Sets-of-5-Words-4x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 3 --sm 0.5
+# ./render.pl -i Sets-of-5-Words-3x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 2 --sm 0.8
+# ./render.pl -i Sets-of-5-Words-2x-Word-Spacing.txt -s 15 17 20 22 25 28 30 35 40 45 50 -x 1 --sm 1.0
 
 my @lines = <$in>;
 close $in;
